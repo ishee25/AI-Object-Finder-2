21d5f65d76aaae3f8f4c9ea9a4b0651fb908bd40
ishee25 <74249367+ishee25@users.noreply.github.com>
2021-05-09 20:35:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    objectName = document.getElementById("object_name_input").value;
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+}
